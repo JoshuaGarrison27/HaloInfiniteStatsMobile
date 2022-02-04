b821cf6f907153db4e9405d807b8b1f05ab4163b
JoshuaGarrison27 <joshuagarrison27@gmail.com>
2022-02-03 21:05:42 -0500

    Minor Fixes

--- HaloInfiniteMobileApp/HaloInfiniteMobileApp/Services/HaloInfiniteService.cs
+++ HaloInfiniteMobileApp/HaloInfiniteMobileApp/Services/HaloInfiniteService.cs
@@ -15,6 +15,11 @@ namespace HaloInfiniteMobileApp.Services
 
         public HaloInfiniteService()
         {
+            if(Secrets.HaloApiToken == null)
+            {
+                throw new InvalidOperationException($"{nameof(Secrets.HaloApiToken)} is null or empty. This is not valid for this operation.");
+            }
+
             _genericRepository = DependencyService.Get<IGenericRepository>();
             _haloApiAuthToken = Secrets.HaloApiToken;
         }
